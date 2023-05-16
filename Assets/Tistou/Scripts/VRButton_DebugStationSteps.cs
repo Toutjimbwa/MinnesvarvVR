c9bf6a4c1b7a5543ed490c915dbc1e10cd12ee35
Tistou Blomberg <tistou.blomberg@gmail.com>
2023-05-16 20:50:30 +0200

    Hela varvet nästan basic funktionalitet

---
+++ Assets/Tistou/Scripts/VRButton_DebugStationSteps.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using VRTK;
+
+namespace TistouVR
+{
+	public class VRButton_DebugStationSteps : VRTK_InteractableObject
+	{
+		public Animator animator;
+		public UnityEvent myEvent;
+
+		public override void StartUsing(VRTK_InteractUse usingObject)
+		{
+			PressButton();
+		}
+
+		private void PressButton()
+		{
+			animator.SetTrigger("press");
+			myEvent.Invoke();
+		}
+	}
+}
